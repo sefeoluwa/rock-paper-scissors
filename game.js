b1000fc04a3568dbdcf97445c36f3026b1dee98b
sefeoluwa <sefeoluwa01@gmail.com>
2022-10-15 21:01:37 +0100

    Code outline written in preparation for-pseudocode

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+//Declare object Rock
+//Declare object Paper
+//Declare object Scissors
+//Create function to return a random choice between rock, paper, and scissors
+//Create function for one game round - it should have two case sensitive parameters to take computer selection and player selection. The function should return a string that declares a winner. 
+//Create a new function that loops the game-round-function 5 times and logs results in console
+//Use prompt to get user input 
