64a129669da6b235615ba019d52685e0c3560024
sefeoluwa <sefeoluwa01@gmail.com>
2022-10-27 11:51:35 +0100

    Object rock, paper, and scissors declared.

--- game.js
+++ game.js
@@ -1,7 +1,11 @@
 //Declare object Rock
+let rock;
 //Declare object Paper
+let paper;
 //Declare object Scissors
+let scissors;
 //Create function to return a random choice between rock, paper, and scissors
+
 //Create function for one game round - it should have two case sensitive parameters to take computer selection and player selection. The function should return a string that declares a winner. 
 //Create a new function that loops the game-round-function 5 times and logs results in console
 //Use prompt to get user input 
