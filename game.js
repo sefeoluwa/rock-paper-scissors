da673c9fe13452ac60dd62bebf9bb4e8f477aa4a
sefeoluwa <sefeoluwa01@gmail.com>
2022-11-01 11:18:11 +0100

    function for a game round declared but it doesn't save results yet.

--- game.js
+++ game.js
@@ -7,11 +7,33 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random()*choices.length)] 
 }
 
+//Use prompt to get user input
+const getPlayerChoice = prompt("Choose rock, paper, or scissors" , "rock");
 
-//Create function for one game round - it should have two case sensitive parameters to take computer selection and player selection. The function should return a string that declares a winner. 
+//Create function for one game round. The function should return a string that declares a winner. 
+function playARound(playerChoice, computerChoice){
+    if (playerChoice === computerChoice){
+        return ("It's a tie!!!")
+    }
+
+    if (playerChoice === "rock"){
+        return ("You win!!!")
+     }
+        if (playerChoice === "scissors"){
+            return ("You win!!!")
+        }
+    
+    else {
+        return ("You lose! Paper beats rock")
+    }
+}
+
+let playerChoice = getPlayerChoice;
+let computerChoice = getComputerChoice();
+console.log(playARound(playerChoice, computerChoice));
 
 
 //Create a new function that loops the game-round-function 5 times and logs results in console
 
 
-//Use prompt to get user input 
+ 
